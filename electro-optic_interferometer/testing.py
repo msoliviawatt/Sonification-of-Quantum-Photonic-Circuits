cc2678aab82eeeb0a14c99a3b0d7fce055f474ca
Olivia <msoliviawatt@gmail.com>
2026-05-01 11:50:30 -0400

    i like these plots

--- electro-optic_interferometer/testing.py
+++ electro-optic_interferometer/testing.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 from models import RabiModel, MachZehnderModel, PhaseShifterModel, ResonatorModel
 
 def main():
+    fig, axes = plt.subplots(2, 2, figsize = (10, 6))
+    plot_1 = axes[0, 0]
+    plot_2 = axes[0, 1]
+    plot_3 = axes[1, 0]
+    plot_4 = axes[1, 1]
     # test rabi
     t = np.linspace(0, 4, 1000)
     rabi = RabiModel()
@@ -12,15 +17,13 @@ def main():
     detuning = 0.0
     prob_g, prob_e = rabi.probabilities(t, omega, detuning)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(t, prob_g, label = "Ground State Probability")
-    plt.plot(t, prob_e, label = "Excited State Probability")
-    plt.xlabel("Time")
-    plt.ylabel("Probability")
-    plt.title("Rabi Oscillations")
-    plt.legend()
-    plt.grid()
-    plt.show()
+    plot_1.plot(t, prob_g, label = "Ground State Probability")
+    plot_1.plot(t, prob_e, label = "Excited State Probability")
+    plot_1.set_xlabel("Time")
+    plot_1.set_ylabel("Probability")
+    plot_1.set_title("Rabi Oscillations")
+    plot_1.legend()
+    plot_1.grid()
 
     # test mzi
     mzi = MachZehnderModel()
@@ -28,15 +31,13 @@ def main():
     phase = np.linspace(0, 2 * np.pi, 1000)
     prob_0, prob_1 = mzi.output_probabilities(phase)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(phase, prob_0, label = "Output 0")
-    plt.plot(phase, prob_1, label = "Output 1")
-    plt.xlabel("Phase")
-    plt.ylabel("Probabilitiy")
-    plt.title("Mach Zehnder Interferometer")
-    plt.legend()
-    plt.grid()
-    plt.show()
+    plot_2.plot(phase, prob_0, label = "Output 0")
+    plot_2.plot(phase, prob_1, label = "Output 1")
+    plot_2.set_xlabel("Phase")
+    plot_2.set_ylabel("Probabilitiy")
+    plot_2.set_title("Mach Zehnder Interferometer")
+    plot_2.legend()
+    plot_2.grid()
 
     # test phase shifter
     v_for_pi = 5.0
@@ -44,13 +45,11 @@ def main():
     voltage = np.linspace(0, 10, 1000)
     phase_from_v = phase_shifter.phase_from_voltage(voltage)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(voltage, phase_from_v)
-    plt.xlabel("Voltage")
-    plt.ylabel("Phase")
-    plt.title("Voltage to Optical Phase")
-    plt.grid()
-    plt.show()
+    plot_3.plot(voltage, phase_from_v)
+    plot_3.set_xlabel("Voltage")
+    plot_3.set_ylabel("Phase")
+    plot_3.set_title("Voltage to Optical Phase")
+    plot_3.grid()
 
     # test resonator
     resonator = ResonatorModel()
@@ -59,12 +58,14 @@ def main():
     q_factor = 20
     gain = resonator.enhancement(drive_frequency, resonance_frequency, q_factor)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(drive_frequency, gain)
-    plt.xlabel("Drive Frequency")
-    plt.ylabel("Enhancement")
-    plt.title("Mechanical Resonance Enhancement")
-    plt.grid()
+    plot_4.plot(drive_frequency, gain)
+    plot_4.set_xlabel("Drive Frequency")
+    plot_4.set_ylabel("Enhancement")
+    plot_4.set_title("Mechanical Resonance Enhancement")
+    plot_4.grid()
+
+    # show the plots
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
